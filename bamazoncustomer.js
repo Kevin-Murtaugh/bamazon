e7b2ce5f27fdaf0f1e7b2d6486771b9608a6c9f1
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-04-30 07:34:09 -0400

    termination works now

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,13 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var bamazonCRUD = require("bamazonCRUD");
 
-//var validInput = false;
-//var globalProd =[];
-//var product= [];
-//var productChoice = [];
 var stockQtyChoice = 99;
-// var stockChoiceQty = 199;
 var saleAmount= 0;
 var userChoicePrice = 0;
 var userQty = 5;
@@ -30,27 +24,24 @@ var connection = mysql.createConnection({
     console.log("Hello Customer, how are you?  You are logged in today as ID# " + connection.threadId + "\n");
     console.log("Here is what we have for sale today:")
     readProducts();
-    console.log("call buyChoice()");
-    buyChoice();
+//    console.log("call buyChoice()");
+//    buyChoice();
 //    prodChoicefn();
-    prodQtyfn();
+//    prodQtyFn();
 //    connection.end();
   });
 // };
   
 
-  // ebay-like code from exercises
+//print inventory table
   function readProducts() {
    connection.query("SELECT * FROM products", function(err, res) {
       if (err) {
           console.log("err", err);
-  
-      // Log all results of the SELECT statement
       } else {
           for (var i=0; i < res.length; i++){
             console.log("-----------------------------------------------");
             console.log(" | ", res[i].id," | ", res[i].prodName," | ", res[i].deptName, " | ", res[i].price, " | ",  res[i].stkQty, " | ");
- //           console.log("holdArray", holdArray[i]);
           }
           console.log("----------------------------------------------");
         inquirer.prompt([
@@ -61,15 +52,21 @@ var connection = mysql.createConnection({
         choices: function () {
             let choiceArray = [];
             res.forEach(products => choiceArray.push(products.prodName));
+            choiceArray[10] = "quit";
             return choiceArray;
             }
         }
     ])
     .then(function(answer) {
-        // store db row for chosen product in productChoice
-//        console.log("answer.product_name ", answer.product_name);
+        prodQtyFn();
+        // store values globally to accommodate inquirer
         for (var i = 0; i < res.length; i++) {
 //            console.log("res[i]", res[i]);
+            if (answer.product_name === "quit") {
+                console.log("Thank you for shopping at Bamazon.  Goodbye.");
+                connection.end();
+                process.exit();
+            }
             if (res[i].prodName === answer.product_name) {
                 idChoice = i+1;
                 userChoicePrice = res[i].price;
@@ -85,53 +82,14 @@ var connection = mysql.createConnection({
     })
 }
 
-  function prodChoicefn(){
-//     inquirer.prompt([
-//         {
-//         name: "product_name",
-//         type: "list",
-//         message: "Which product would you like to buy?",
-//         choices: function () {
-//             let choiceArray = [];
-//             res.forEach(products => choiceArray.push(products.prodName));
-//             return choiceArray;
-//             }
-//         }
-//     ])
-//     .then(function(answer) {
-//         // store db row for chosen product in productChoice
-// //        console.log("answer.product_name ", answer.product_name);
-//         for (var i = 0; i < res.length; i++) {
-// //            console.log("res[i]", res[i]);
-//             if (res[i].prodName === answer.product_name) {
-//                 idChoice = i+1;
-//                 userChoicePrice = res[i].price;
-//                 productChoice = res[i].prodName;
-//                 stockQtyChoice = res[i].stkQty;
-//                 console.log("inside 1st .then", i, idChoice, userChoicePrice, productChoice, stockQtyChoice);
-//             }
-//         }
-//     });
-};
 
-function prodQtyfn() {
+function prodQtyFn() {
         inquirer.prompt([
           {
             type: "input",
             name: "userQty",
             message: "How many would you like to buy?"
           }, 
-             // Up to ", stockQtyChoice, " available."
-            // validate: function(userQty) {                
-            //     if (userQty <= stockQtyChoice) {
-            //         console.log("userQty ", userQty, "stockQtyChoice ", stockQtyChoice); 
-            //         stockChoiceQty -= userQty;
-            //     } else {
-            //         return "Sorry, we do not have enough stock to satisfy your order.";
-            //     }
-          //  validate: function(input) {
-            //   // check against inventory available; if user asks for too much, prompt won't continue
-//            return input <= productChoice.stkQty || "Sorry, we don't have that many in stock. Try again!";
         ])
         .then(function(answer) {
             console.log(answer, "in 2nd .then: userQty", userQty, "idChoice", idChoice, "userChoicePrice", userChoicePrice);
@@ -142,14 +100,7 @@ function prodQtyfn() {
                 updateDB();
             }
             saleAmount = userChoicePrice * userQty;
-//           var query = "UPDATE products SET stkQty= ?, WHERE id= ?";
-//           var values = 
-//             stockQtyChoice,
-//  //           productChoice.product_sales + saleAmount,
-//             idChoice
-//           ];
-//           console.log("saleAmount:", saleAmount, "query", query, "values", values);
-//           connection.query(query, values, function(err){
+
     function updateDB() {
         console.log("stockQtyChoice", stockQtyChoice, "UserQty ", userQty);
             connection.query(
@@ -166,7 +117,11 @@ function prodQtyfn() {
               if (err) throw err;
             console.log("Order placed!");
           console.log("Total cost = " + userQty + " units at $" + userChoicePrice + "= $" + saleAmount);
+          console.log("Would you like to continue?");
+          
+
           readProducts();
+          
     
  //         governAll();
             }
@@ -178,9 +133,9 @@ function prodQtyfn() {
  //   });
 
 
-function buyChoice() {
-    // fetch and display db data
-    connection.query("SELECT * FROM products ORDER BY id", function (err, res) {
-        if (err) throw err;
-    });
-  };
+// function buyChoice() {
+//     // fetch and display db data
+//     connection.query("SELECT * FROM products ORDER BY id", function (err, res) {
+//         if (err) throw err;
+//     });
+//   };
